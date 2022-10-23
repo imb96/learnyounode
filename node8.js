3c6fe0d7bdec5a2d1edeb84e15949e0f70c04f60
imb96 <kimminje7267@naver.com>
2022-10-23 17:00:28 +0900

    HTTP COLLECT

---
+++ node8.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const bl = require("bl");
+const url = process.argv[2];
+
+http
+  .get(url, function (response) {
+    response.pipe(
+      bl(function (err, data) {
+        if (err) return console.error(err);
+        data = data.toString();
+        console.log(data.length);
+        console.log(data);
+      })
+    );
+  })
+  .on("error", console.error);
